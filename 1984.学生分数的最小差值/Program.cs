8ec6009d6183a7304e86d8794e95ab477dcf68fd
kuuhakumirai <34931606+kuuhakumirai@users.noreply.github.com>
2022-02-13 04:49:52 +0800

    2/13

---
+++ 1984.学生分数的最小差值/Program.cs
@@ -0,0 +1,16 @@
+﻿int MinimumDifference(int[] nums, int k) 
+{
+    List<int> numGroups = nums.OrderBy(stu => stu).ToList();
+
+    int count = numGroups.Count;
+
+    int result = numGroups[count - 1];
+
+    for (int i = 0; i + k - 1 < count; ++i)
+    {
+            result = Math.Min(result, numGroups[i + k - 1] - numGroups[i]);
+    }
+    
+    return result;
+
+}
