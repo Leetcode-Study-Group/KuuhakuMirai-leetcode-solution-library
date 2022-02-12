e1253a6f72a34b8aa05189f7d400dc65d82991d1
kuuhakumirai <34931606+kuuhakumirai@users.noreply.github.com>
2022-02-13 04:42:43 +0800

    2/13

---
+++ 1189. “气球” 的最大数量/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(MaxNumberOfBalloons("nlaebolko"));
+
+
+int MaxNumberOfBalloons(string text)
+{
+    Dictionary<char, int> dic = new()
+    {
+        ['b'] = 0,
+        ['a'] = 0,
+        ['l'] = 0,
+        ['o'] = 0,
+        ['n'] = 0,
+    };
+
+    foreach (var letter in text)
+    {
+        if (dic.ContainsKey(letter))
+        {
+            dic[letter]++;
+        }
+    }
+
+    dic['l'] /= 2;
+    dic['o'] /= 2;
+
+    char min = dic.OrderBy(key => key.Value).Select(min => min.Key).First();
+
+    return dic[min];
+}
