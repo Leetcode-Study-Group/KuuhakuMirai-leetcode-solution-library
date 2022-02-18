ee0c0e3528692f568b9ec14117a48e14584fdb0e
kuuhakumirai <34931606+kuuhakumirai@users.noreply.github.com>
2022-02-18 15:03:41 +0800

    Update Program.cs

--- 1791. 找出星型图的中心节点/Program.cs
+++ 1791. 找出星型图的中心节点/Program.cs
@@ -1,12 +1,4 @@
 int FindCenter(int[][] edges) 
 {
-    List<int> item = new();
-    
-    for (int i = 0; i < edges.Length - 1; i++)
-    {
-        item = edges[i].Intersect(edges[i + 1]).ToList();
-    }
-        
-    return item.FirstOrDefault();
-    
+    return edges[0].Intersect(edges[1]).FirstOrDefault();    
 }
